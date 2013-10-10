943efb3c2a343c4ec7fb536e9186d14a42ba4f00
James Ottaway <git@james.ottaway.io>
2013-10-10 21:20:18 +1000

    don't need this any more

--- test.js
+++
@@ -1,9 +0,0 @@
-var someday = require('./index');
-
-new someday({ timeout: 5 }).do(function() {
-	console.log('winning!')
-}).when(function() {
-	var random = Math.random();
-	console.log(random);
-	return random < 0.01;
-});
